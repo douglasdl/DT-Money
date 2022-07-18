dbbd367d27d0d4cce1ed47412867a46c492855bf
douglasdl <douglas_san@hotmail.com>
2022-07-18 00:53:49 -0300

    Update layout

--- dtmoney/src/components/TransactionsTable/styles.ts
+++ dtmoney/src/components/TransactionsTable/styles.ts
@@ -1,11 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-    margin-top: 4rem;
+    margin-top: -3rem;
     
     table {
         width: 100%;
         border-spacing: 0 0.5rem;
+        max-width: 1120px;
+        margin: 0 auto;
+        padding: 1.5rem 1rem;
 
         th {
             color: var(--text-body);
